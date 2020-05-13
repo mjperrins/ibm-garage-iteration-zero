8489e7f8ab063de5d3ee3f1fcf38f5836e422e60
Matthew Perrins <mjperrin@us.ibm.com>
2020-05-13 11:07:03 -0500

    Update terraform syntax

--- terraform/stages/stage1-cluster.tf
+++ terraform/stages/stage1-cluster.tf
@@ -1,19 +1,20 @@
 module "dev_cluster" {
   source = "github.com/seansund/garage-terraform-modules.git//cloud-managed/cluster/ibmcloud?ref=private-catalog"
 
-  resource_group_name     = var.resource_group_name
-  cluster_name            = var.cluster_name
-  private_vlan_id         = var.private_vlan_id
-  public_vlan_id          = var.public_vlan_id
-  vlan_datacenter         = var.vlan_datacenter
-  cluster_region          = var.vlan_region
-  kubeconfig_download_dir = var.user_home_dir
-  cluster_machine_type    = var.cluster_machine_type
-  cluster_worker_count    = var.cluster_worker_count
-  cluster_hardware        = var.cluster_hardware
-  cluster_type            = var.cluster_type
-  cluster_exists          = var.cluster_exists
-  ibmcloud_api_key        = var.ibmcloud_api_key
-  name_prefix             = var.name_prefix
-  is_vpc                  = var.vpc_cluster
+  resource_group_name     = "${var.resource_group_name}"
+  cluster_name            = "${var.cluster_name}"
+  private_vlan_id         = "${var.private_vlan_id}"
+  public_vlan_id          = "${var.public_vlan_id}"
+  vlan_datacenter         = "${var.vlan_datacenter}"
+  cluster_region          = "${var.vlan_region}"
+  kubeconfig_download_dir = "${var.user_home_dir}"
+  cluster_machine_type    = "${var.cluster_machine_type}"
+  cluster_worker_count    = "${var.cluster_worker_count}"
+  cluster_hardware        = "${var.cluster_hardware}"
+  cluster_type            = "${var.cluster_type}"
+  cluster_exists          = "${var.cluster_exists}"
+  ibmcloud_api_key        = "${var.ibmcloud_api_key}"
+  name_prefix             = "${var.name_prefix}"
+  is_vpc                  = "${var.vpc_cluster}"
+
 }
